dd3a433413d352c84cbb6f5761c4c93b24ff308b
Zidane Innis <zidaneinnis@gmail.com>
2022-10-09 21:34:54 -0500

    Completed Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
@@ -6,17 +7,41 @@ type Props = {};
 
 function Hero({}: Props) {
   const [text, count] = useTypewriter({
-    words: ["My name is Zidane Innis", "ReactJS Developer"],
+    words: ["My name is Zidane Innis", "React.JS Developer"],
     loop: true,
     delaySpeed: 2000,
   });
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-8 overflow-hidden text-center">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <img
+        className="object-cover w-32 h-32 mx-auto rounded-full"
+        src="https://media-exp1.licdn.com/dms/image/C5603AQFwxOmluAqhbw/profile-displayphoto-shrink_800_800/0/1661888341256?e=1671062400&v=beta&t=822RToVHXbUMClvfhlLPasZKt51ZxQK9LXf0wIa1t1k"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="pb-2 text-sm uppercase tracking-[15px] text-gray-500 ">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl font-semibold lg:text-6xl scroll-px10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+      </div>
+      <div className="pt-5">
+        <Link href="#about">
+          <button className="heroButton">About</button>
+        </Link>
+        <Link href="#experience">
+          <button className="heroButton">Experience</button>
+        </Link>
+        <Link href="#skills">
+          <button className="heroButton">Skills</button>
+        </Link>
+        <Link href="#projects">
+          <button className="heroButton">Projects</button>
+        </Link>
+      </div>
     </div>
   );
 }
