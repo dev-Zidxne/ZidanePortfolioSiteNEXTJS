ce1b1967f7d85ae9e3cc1d02bb71c83caeb60319
Zidane Innis <zidaneinnis@gmail.com>
2022-10-09 21:01:09 -0500

    Building hero section 2/2

---
+++ components/Hero.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+
+type Props = {};
+
+function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: ["My name is Zidane Innis", "ReactJS Developer"],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="flex flex-col items-center justify-center h-screen space-y-8 overflow-hidden text-center">
+      <BackgroundCircles />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor="#F7AB0A" />
+      </h1>
+    </div>
+  );
+}
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,19 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Zidane's Portfolio</title>
       </Head>
       <Header />
-
-      <h1 className="p-10 text-blue-500"></h1>
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
     </div>
   );
 };
