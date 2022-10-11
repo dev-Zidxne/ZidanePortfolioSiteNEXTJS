1bef0fa2e82de1846a8e3b03899ee50df526f7e7
Zidane Innis <zidaneinnis@gmail.com>
2022-10-10 23:41:09 -0500

    Building the Experience Cards in the Work Experience Section

---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,41 @@
+import { MotionConfig } from "framer-motion";
+import React from "react";
+import { motion } from "framer-motion";
+type Props = {};
+
+function ExperienceCard({}: Props) {
+  return (
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[520px] md:w-[600px] xl:w-[900px]">
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        className="w-32 h-32 rounded-full xl:w-[200px] object-cover"
+        src="https://media-exp1.licdn.com/dms/image/C5603AQFwxOmluAqhbw/profile-displayphoto-shrink_800_800/0/1661888341256?e=1671062400&v=beta&t=822RToVHXbUMClvfhlLPasZKt51ZxQK9LXf0wIa1t1k"
+      />
+      <div className="px-0 md:px-10">
+        <h4 className="text-4xl font-light">CEO OF </h4>
+        <p className="mt-2 text-2xl font-bold">Zidane</p>
+        <div className="w-10 h-10 rounded-full">
+          <img src="" />
+          <img src="" />
+          <img src="" />
+          <img src="" />
+        </div>
+        <p> Started.. Ended...</p>
+        <ul className="ml-5 space-y-4 text-lg list-disc">
+          <li>Summary Points</li>
+          <li>Summary Points</li>
+          <li>Summary Points</li>
+          <li>Summary Points</li>
+          <li>Summary Points</li>
+        </ul>
+      </div>
+    </article>
+  );
+}
+
+export default ExperienceCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import About from "../components/About";
+import ExperienceCard from "../components/ExperienceCard";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import WorkExperience from "../components/WorkExperience";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -18,6 +20,9 @@ const Home: NextPage = () => {
       </section>
       <section id="about" className="snap-center">
         <About />
+      </section>
+      <section id="experience" className="snap-center">
+        <WorkExperience />
       </section>
     </div>
   );
