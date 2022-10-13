a77eaa6b05c7ddde3f268378b8122f0ba1d8f7ea
Zidane Innis <zidaneinnis@gmail.com>
2022-10-12 20:52:31 -0500

    Building Skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 type Props = {};
 
 export default function Skills({}: Props) {
-  return <div>Skills</div>;
+  return (
+    <motion.div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[200px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+      <h3 className="absolute top-24 upper tracking-[20px] text-gray-500 text-2xl">
+        Skill
+      </h3>
+    </motion.div>
+  );
 }
