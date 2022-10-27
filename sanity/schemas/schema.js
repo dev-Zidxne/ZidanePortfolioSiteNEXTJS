72c4d37ad0eebc4d8580c00aadbaa8c40928d552
Zidane Innis <zidaneinnis@gmail.com>
2022-10-27 15:18:32 -0500

    Implementing sanity sand its schemas

---
+++ sanity/schemas/schema.js
@@ -0,0 +1,16 @@
+// First, we must import the schema creator
+import createSchema from "part:@sanity/base/schema-creator";
+
+import schemaTypes from "all:part:@sanity/base/schema-type";
+
+import skill from "./skill";
+import pageInfo from "./pageInfo";
+import experience from "./experience";
+import project from "./project";
+import social from "./social";
+
+export default createSchema({
+  name: "default",
+
+  types: schemaTypes.concat([skill, pageInfo, experience, social, project]),
+});
