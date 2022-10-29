a4e294d64c025ba5b4a156ad7c7975e4ca22250f
Zidane Innis <zidaneinnis@gmail.com>
2022-10-29 12:54:50 -0500

    Updates to Website

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,62 +28,61 @@ type Props = {
 
 const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
   return (
-    <h2>Hello</h2>
-    // <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden  z-0  scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
-    //   <Head>
-    //     <title>{pageInfo?.name}</title>
-    //   </Head>
-    //   <Header socials={socials} />
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden  z-0  scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      <Head>
+        <title>{pageInfo?.name}</title>
+      </Head>
+      <Header socials={socials} />
 
-    //   <section id="hero" className="snap-start">
-    //     <Hero pageInfo={pageInfo} />
-    //   </section>
-    //   <section id="about" className="snap-center">
-    //     <About pageInfo={pageInfo} />
-    //   </section>
-    //   <section id="experience" className="snap-center">
-    //     <WorkExperience experiences={experiences} />
-    //   </section>
-    //   <section id="skills" className="snap-start">
-    //     <Skills skills={skills} />
-    //   </section>
-    //   <section id="projects" className="snap-start">
-    //     <Projects projects={projects} />
-    //   </section>
-    //   <section id="contact" className="snap-start">
-    //     <ContactMe pageInfo={pageInfo} />
-    //   </section>
-    //   <Link href="#hero">
-    //     <footer className="sticky w-full cursor-pointer bottom-5">
-    //       <div className="flex items-center justify-center">
-    //         <img
-    //           className="w-10 h-10 rounded-full cursor-pointer filter grayscale hover:grayscale-0 "
-    //           src="https://media-exp1.licdn.com/dms/image/D4E03AQHdJZFLTFQDHQ/profile-displayphoto-shrink_800_800/0/1665970228383?e=1671667200&v=beta&t=wKKVw1mofVg899wMOc5pVB5suTg4nD0Vz8PWKdjtz6w"
-    //           alt=""
-    //         ></img>
-    //       </div>
-    //     </footer>
-    //   </Link>
-    // </div>
+      <section id="hero" className="snap-start">
+        <Hero pageInfo={pageInfo} />
+      </section>
+      <section id="about" className="snap-center">
+        <About pageInfo={pageInfo} />
+      </section>
+      <section id="experience" className="snap-center">
+        <WorkExperience experiences={experiences} />
+      </section>
+      <section id="skills" className="snap-start">
+        <Skills skills={skills} />
+      </section>
+      <section id="projects" className="snap-start">
+        <Projects projects={projects} />
+      </section>
+      <section id="contact" className="snap-start">
+        <ContactMe pageInfo={pageInfo} />
+      </section>
+      <Link href="#hero">
+        <footer className="sticky w-full cursor-pointer bottom-5">
+          <div className="flex items-center justify-center">
+            <img
+              className="w-10 h-10 rounded-full cursor-pointer filter grayscale hover:grayscale-0 "
+              src="https://media-exp1.licdn.com/dms/image/D4E03AQHdJZFLTFQDHQ/profile-displayphoto-shrink_800_800/0/1665970228383?e=1671667200&v=beta&t=wKKVw1mofVg899wMOc5pVB5suTg4nD0Vz8PWKdjtz6w"
+              alt=""
+            ></img>
+          </div>
+        </footer>
+      </Link>
+    </div>
   );
 };
 
 export default Home;
 
-// export const getStaticProps: GetStaticProps<Props> = async () => {
-//   const pageInfo: PageInfo = await fetchPageInfo();
-//   const experiences: Experience[] = await fetchExperience();
-//   const skills: Skill[] = await fetchSkills();
-//   const projects: Project[] = await fetchProjects();
-//   const socials: Social[] = await fetchSocials();
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperience();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
 
-//   return {
-//     props: {
-//       pageInfo,
-//       experiences,
-//       skills,
-//       projects,
-//       socials,
-//     },
-//   };
-// };
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+  };
+};
