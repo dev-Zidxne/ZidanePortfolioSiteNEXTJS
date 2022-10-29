540b34dc89ec6bce142104dee84cce366191c6ca
Zidane Innis <zidaneinnis@gmail.com>
2022-10-29 16:22:05 -0500

    Updates

--- sanity.ts
+++ sanity.ts
@@ -1,16 +1,10 @@
 import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
-// export const config = {
-//   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-//   projectId: "eivssvow",
-//   apiVersion: "2021-08-11",
-//   useCdn: process.env.NODE_ENV === "production",
-// };
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  apiVersion: "2021-08-11", // or today's date for latest
+  apiVersion: "2021-10-29", // or today's date for latest
 
   useCdn: process.env.NODE_ENV === "production",
 };
