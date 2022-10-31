1a6b894b34d0a2c3f3489cf6e0384a29d1ddc628
Zidane Innis <zidaneinnis@gmail.com>
2022-10-31 03:07:49 -0500

    Up

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -45,7 +45,7 @@ function ContactMe({ pageInfo }: Props) {
   };
 
   return (
-    <div className="relative bottom-0 flex items-center h-screen text-center md:text-left md:flex-row justify-evenly overscroll-x-none ">
+    <div className="relative bottom-0 flex items-center h-screen text-center md:text-left md:flex-row justify-evenly ">
       <h3 className="absolute uppercase top-16 tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ type Props = {
 
 const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y overflow-y-scroll overscroll-x-scroll z-0  scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overscroll-x-scroll z-0  scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>{pageInfo?.name}</title>
       </Head>
