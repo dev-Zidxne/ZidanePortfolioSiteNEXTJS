54a3c07bfda130d3900d5a1592ad15ff77495cca
Zidane Innis <zidaneinnis@gmail.com>
2022-10-31 02:28:08 -0500

    Upity

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -45,7 +45,7 @@ function ContactMe({ pageInfo }: Props) {
   };
 
   return (
-    <div className="relative bottom-0 flex flex-col items-center h-screen m-auto text-center md:text-left md:flex-row justify-evenly ">
+    <div className="relative bottom-0 flex items-center h-screen text-center md:text-left md:flex-row justify-evenly ">
       <h3 className="absolute uppercase top-16 tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
@@ -102,7 +102,7 @@ function ContactMe({ pageInfo }: Props) {
 
         <form
           onSubmit={sendEmail}
-          className="flex flex-col w-screen p-5 space-y-2 md:w-fit "
+          className="flex flex-col w-screen p-5 space-y-2 md:w-fit h-96"
         >
           {!isTabletOrMobile && (
             <div className="space-x-2 ">
