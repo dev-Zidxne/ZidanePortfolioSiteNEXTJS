a8dd53b8b28d4e1913c16e5ac74b4b5556050aa0
Zidane Innis <zidaneinnis@gmail.com>
2022-10-31 01:59:09 -0500

    Update

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -45,7 +45,7 @@ function ContactMe({ pageInfo }: Props) {
   };
 
   return (
-    <div className="relative flex flex-col items-center w-screen h-screen m-auto text-center md:text-left md:flex-row justify-evenly ">
+    <div className="relative flex flex-col items-center w-screen h-screen m-auto overflow-scroll overflow-x-hidden text-center md:text-left md:flex-row justify-evenly">
       <h3 className="absolute uppercase top-16 tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
