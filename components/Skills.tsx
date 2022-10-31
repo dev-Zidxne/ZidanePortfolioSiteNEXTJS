e8ea2692d3e0894bb6ae22d8394944ce5908abae
Zidane Innis <zidaneinnis@gmail.com>
2022-10-30 23:41:50 -0500

    Fixed skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -17,7 +17,7 @@ export default function Skills({ skills }: Props) {
       <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
       </h3>
-      <h3 className="absolute top-28 upper tracking-[3px] text-gray-500 text-md m-1">
+      <h3 className="absolute top-24 upper tracking-[3px] text-gray-500 text-md m-1">
         Hover over a skill for current proficiency.
       </h3>
       {!isTabletOrMobile && (
@@ -32,7 +32,7 @@ export default function Skills({ skills }: Props) {
       )}
 
       {isTabletOrMobile && (
-        <div className="grid grid-cols-3 gap-3 mt-20 overflow-x-hidden overflow-x-scroll overflow-y-scroll overscroll-y-none">
+        <div className="grid grid-cols-3 gap-3 mt-16 overflow-x-hidden overflow-x-scroll overflow-y-scroll overscroll-y-none">
           {skills?.slice(0, skills.length / 2).map((skill) => (
             <Skill key={skill._id} skill={skill} />
           ))}
