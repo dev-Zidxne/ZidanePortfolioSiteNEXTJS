20b5db67080afae3b6016d77066c079055deb172
Zidane Innis <zidaneinnis@gmail.com>
2022-11-01 04:11:15 -0500

    Adjsutments

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -15,11 +15,11 @@ export default function Skills({ skills }: Props) {
       transition={{ duration: 1.5 }}
       className="relative flex text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 h-screen xl:space-y-0 mx-auto items-center justify-center "
     >
-      <h3 className="absolute uppercase top-8  ml-6 tracking-[20px] text-gray-500 text-2xl lg:top-48 lg:text-4xl">
+      <h3 className="absolute uppercase top-6  ml-6 tracking-[20px] text-gray-500 text-2xl lg:top-48 lg:text-4xl">
         Skills
       </h3>
 
-      <h3 className="absolute top-20 tracking-[3px] text-gray-500 text-sm lg:text-xl lg:top-64">
+      <h3 className="absolute top-16 tracking-[3px] text-gray-500 text-sm lg:text-xl lg:top-64">
         Hover or tap on a skill for current proficiency.
       </h3>
 
