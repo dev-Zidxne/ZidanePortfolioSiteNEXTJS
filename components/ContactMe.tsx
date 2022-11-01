007d00c8c4530b0f773a7b40f9c1cd3cfb3ebf5a
Zidane Innis <zidaneinnis@gmail.com>
2022-10-31 20:05:23 -0500

    Adjusted margins on page titles

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -45,8 +45,8 @@ function ContactMe({ pageInfo }: Props) {
   };
 
   return (
-    <div className="relative bottom-0 flex items-center justify-center h-screen text-center md:text-left md:flex-row justify-evenly ">
-      <h3 className="absolute uppercase top-8   tracking-[20px] text-gray-500 text-2xl">
+    <div className="relative bottom-0 flex items-center justify-center h-screen text-center md:text-left md:flex-row ">
+      <h3 className="absolute uppercase top-8  ml-6 tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
       <div className="flex flex-col ">
