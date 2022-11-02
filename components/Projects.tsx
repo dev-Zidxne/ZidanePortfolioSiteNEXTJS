00e01cc9dfae4bde0563c2ae4cfd62e5202fa039
Zidane Innis <zidaneinnis@gmail.com>
2022-11-02 01:14:54 -0500

    Project section cutoff fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -19,21 +19,21 @@ export default function Projects({ projects }: Props) {
       transition={{
         duration: 1.5,
       }}
-      className="relative flex flex-col items-center h-screen p-0 mx-auto  md:text-left md:flex-row max-w-screen justify-evenly  space-y-7 xl:h-[700px] top-10"
+      className="relative flex flex-col items-center h-screen p-10 mx-auto  md:text-left md:flex-row max-w-screen justify-evenly space-y-7 xl:h-[700px] "
     >
       <h3 className="absolute uppercase top-6  ml-6 tracking-[20px] text-gray-500 text-2xl  lg:text-4xl ">
         Projects
       </h3>
-      <h3 className="absolute top-16 tracking-[2px] text-gray-500 text-sm lg:text-xl lg:top-28  md:top-52 ">
+      <h3 className="absolute top-16 tracking-[2px] text-gray-500 text-sm lg:text-xl lg:top-24 md:top-52 ">
         Click or tap on a project image to visit.
       </h3>
       <div className="relative z-30 flex w-full transition  scrollbar-thin overflow-y-hidden snap-x snap-mandatory hover:scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 bottom-20 ">
         {projects.map((project, i) => (
           <div
             key={project.toString()}
-            className="flex flex-col items-center justify-center flex-shrink-0 w-screen h-screen space-y-5 overflow-y-scroll p- snap-center md:p-44 "
+            className="flex flex-col items-center justify-center flex-shrink-0 w-screen h-screen space-y-5 p- snap-center md:p-44 "
           >
-            <div className="flex-shrink-0 object-cover w-24 h-24   md:mb-0 md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[400px]  cursor-pointer hover:opacity-80 bg-transparent-400 transition ">
+            <div className="flex-shrink-0 object-cover w-24 h-24   md:mb-0 md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[400px] mt-20 cursor-pointer hover:opacity-80 bg-transparent-400 transition ">
               <Link href={project.linkToBuild}>
                 <a target="_blank">
                   <motion.img
@@ -47,7 +47,7 @@ export default function Projects({ projects }: Props) {
                     src={urlFor(project?.image).url()}
                     alt=""
                     className="flex-shrink-0 object-cover  h-32  md:rounded-lg md:w-64 md:h-56 xl:w-[500px] xl:h-[200px]  xl:mt-32 
-                   lg:mt-40 cursor-pointer  rounded-md md:mt-40 "
+                   lg:mt-40 cursor-pointer  rounded-md "
                   />
                 </a>
               </Link>
