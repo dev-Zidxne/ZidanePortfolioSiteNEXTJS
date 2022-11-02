eb83a18284340bcf9d0c40f7bc0554334f509f86
Zidane Innis <zidaneinnis@gmail.com>
2022-11-01 19:43:26 -0500

    Projects page adjustments

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -19,12 +19,15 @@ export default function Projects({ projects }: Props) {
       transition={{
         duration: 1.5,
       }}
-      className="relative flex flex-col items-center h-screen px-10 mx-auto md:text-left md:flex-row max-w-7xl justify-evenly "
+      className="relative flex flex-col items-center h-screen mx-auto md:text-left md:flex-row max-w-screen justify-evenly "
     >
-      <h3 className="absolute uppercase top-24 tracking-[20px] text-gray-500 text-2xl ml-6 lg:text-4xl">
+      <h3 className="absolute uppercase top-6  ml-6 tracking-[20px] text-gray-500 text-2xl lg:top-32 lg:text-4xl">
         Projects
       </h3>
-      <div className="relative z-20 flex w-full overflow-y-hidden snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 bottom-32">
+      <h3 className="absolute top-16 tracking-[2px] text-gray-500 text-sm lg:text-xl lg:top-48">
+        Click or tap on a project image to visit.
+      </h3>
+      <div className="relative z-30 flex w-full overflow-y-hidden snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 bottom-32">
         {projects.map((project, i) => (
           <div
             key={project.toString()}
@@ -48,30 +51,30 @@ export default function Projects({ projects }: Props) {
                 </a>
               </Link>
             </div>
-            <div className="max-w-6xl px-0 space-y-10 md:px-10">
-              <h4 className="text-4xl font-semibold text-center">
-                <span className="underline decoration-[#F7AB0A]/50 text-sm lg:text-lg ">
+            <div className="max-w-6xl px-0 space-y-5 md:px-10">
+              <h4 className="text-4xl font-semibold text-center ">
+                <span className=" underline decoration-[#F7AB0A]/50 text-sm lg:text-lg ">
                   Case Study {i + 1} of {projects.length}: {project?.title}
                 </span>{" "}
               </h4>{" "}
-              <div className="flex items-center justify-center space-x-2">
+              <div className="flex items-center justify-center space-x-2 ">
                 {project?.technologies.map((technology) => (
                   <img
-                    className="h-10 m-1 rounded-full"
+                    className="h-10 rounded-full"
                     key={technology._id}
                     src={urlFor(technology.image).url()}
                     alt=""
                   />
                 ))}
               </div>
-              <p className="text-lg text-center md:text-left ">
+              <p className="text-sm text-center lg:text-lg md:text-left ">
                 {project?.summary}
               </p>
             </div>
           </div>
         ))}
       </div>
-      <div className="w-full absolute top-[20%] bg-[#F7AB0A]/10 left-0 h-[400px] -skew-y-12"></div>
+      <div className="w-full absolute top-[18%] bg-[#F7AB0A]/10 left-0 h-[400px] -skew-y-12"></div>
     </motion.div>
   );
 }
