e63a2422948dc1bc6fc849f820f3df19fd4a1b7a
Zidane Innis <zidaneinnis@gmail.com>
2023-01-18 21:58:42 -0500

    Changed date format code

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -8,6 +8,21 @@ type Props = {
 };
 
 export default function ExperienceCard({ experience }: Props) {
+  const employmentTimeStarted = new Date(
+    experience.dateStarted
+  ).toLocaleDateString("en-US", {
+    month: "long",
+    year: "numeric",
+  });
+
+  const employmentTimeEnded = new Date(experience.dateEnded).toLocaleDateString(
+    "en-US",
+    {
+      month: "long",
+      year: "numeric",
+    }
+  );
+
   return (
     <article className="flex flex-col  rounded-3xl items-center flex-shrink-0 w-[300px]  lg-[500px]  md:w-[600px] xl:w-[600px] xl:h-[700px]   first-letter:  bg-[#292929] p-3 ml-6 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 overflow-hidden ">
       <motion.img
@@ -40,10 +55,8 @@ export default function ExperienceCard({ experience }: Props) {
             ))}
         </div>
         <p className="py-2 text-center text-gray-300 uppercase lg:text-left">
-          {new Date(experience.dateStarted).toDateString()} - {""}
-          {experience.isCurrentlyWorkingHere
-            ? "Present"
-            : new Date(experience.dateEnded).toDateString()}
+          {employmentTimeStarted} - {""}
+          {experience.isCurrentlyWorkingHere ? "Present" : employmentTimeEnded}
         </p>
         {/* <ul className=" h-40 m-14 sm:mx-44 space-y-0.5 text-xs text-left list-disc lg:text-base lg:mx-10 md:mx-32 ">
           {experience.points.map((point, i) => (
