4a485ddc4a1a4f85c3f4fd46f2f6fb5a65906990
Zidane Innis <zidaneinnis@gmail.com>
2023-10-25 02:12:09 -0500

    Added social icon tooltip

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,7 @@ type Props = { pageInfo: PageInfo };
 
 function About({ pageInfo }: Props) {
 	return (
-		<motion.div className="relative flex flex-col items-center justify-center px-10 mx-auto mb-28 md:text-left md:flex-row max-w-7xl top-6 md:top-0 lg:top-10 ">
+		<motion.div className="relative flex flex-col items-center justify-center px-10 mx-auto mb-28 md:text-left md:flex-row max-w-7xl top-6 md:top-0 lg:top-10">
 			<h3 className="absolute uppercase top-8  ml-6 tracking-[20px] text-gray-500 text-2xl lg:text-4xl">
 				About
 			</h3>
@@ -21,7 +21,7 @@ function About({ pageInfo }: Props) {
 				whileInView={{ opacity: 1, x: 0 }}
 				viewport={{ once: true }}
 				src={urlFor(pageInfo?.profilePic).url()}
-				className="flex-shrink-0 object-cover w-56 h-56 rounded-full md:mb-0 md:rounded-3xl md:w-64 md:h-96 xl:w-[400px] xl:h-[500px] mt-20"
+				className="flex-shrink-0 object-cover w-56 h-56 rounded-full md:mb-0 md:rounded-3xl md:w-64 md:h-96 xl:w-[500px] xl:h-[500px] mt-20"
 			/>
 			<div className="space-y-5 md:px-10 text">
 				<h4 className="text-4xl font-semibold text-center ">
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,66 +1,68 @@
-import React from "react";
-import { SocialIcon } from "react-social-icons";
-import { motion } from "framer-motion";
-import Link from "next/link";
-import { Social } from "../typings";
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { Social } from '../typings';
 
 type Props = {
-  socials: Social[];
+	socials: Social[];
 };
 
 export default function Header({ socials }: Props) {
-  return (
-    <header className="sticky top-0 z-50 flex items-start justify-between pl-5 pr-5 mx-auto max-w-7xl xl:items-center backdrop-blur-md back lg:max-w-full lg:justify-around md:max-w-full md:justify-around sm:max-w-full sm:justify-around xs:max-w-full xs:justify-around xl:max-w-full xl:justify-around 2xl:max-w-full 2xl:justify-around">
-      <motion.div
-        initial={{ x: -500, opacity: 0, scale: 0.5 }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 1.5,
-        }}
-        className="flex flex-row items-center"
-      >
-        {" "}
-        {socials.map((social) => (
-          <SocialIcon
-            key={social._id}
-            url={social.url}
-            bgColor="transparent"
-            fgColor="currentColor"
-            className="text-[#808080ff] transition cursor-pointer hover:text-white"
-            target="_blank"
-          />
-        ))}
-      </motion.div>
-      <Link href="#contact">
-        <motion.div
-          initial={{ x: 500, opacity: 0, scale: 0.5 }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            duration: 1.5,
-          }}
-          className="flex flex-row items-center text-gray-300 cursor-pointer "
-        >
-          <div className="tippy-tooltip">
-            <SocialIcon
-              className="text-[#808080ff] transition cursor-pointer hover:text-white "
-              network="email"
-              fgColor="currentColor"
-              bgColor="transparent"
-            />
-          </div>
-          <p className="hidden text-sm text-gray-400 uppercase md:inline-flex">
-            Get In Touch
-          </p>
-        </motion.div>
-      </Link>
-    </header>
-  );
+	return (
+		<header className="sticky top-0 z-50 flex items-start justify-between pl-5 pr-5 mx-auto max-w-7xl xl:items-center backdrop-blur-md back lg:max-w-full lg:justify-around md:max-w-full md:justify-around sm:max-w-full sm:justify-around xs:max-w-full xs:justify-around xl:max-w-full xl:justify-around 2xl:max-w-full 2xl:justify-around">
+			<motion.div
+				initial={{ x: -500, opacity: 0, scale: 0.5 }}
+				animate={{
+					x: 0,
+					opacity: 1,
+					scale: 1,
+				}}
+				transition={{
+					duration: 1.5,
+				}}
+				className="flex flex-row items-center"
+			>
+				{socials.map((social) => (
+					<div className="social-icon-wrapper" key={social._id}>
+						<SocialIcon
+							key={social._id}
+							url={social.url}
+							bgColor="transparent"
+							fgColor="currentColor"
+							className="text-[#808080ff] transition cursor-pointer hover:text-white"
+							target="_blank"
+						/>
+						<span className="tooltip-text">{social.title}</span>
+					</div>
+				))}
+			</motion.div>
+			<Link href="#contact">
+				<motion.div
+					initial={{ x: 500, opacity: 0, scale: 0.5 }}
+					animate={{
+						x: 0,
+						opacity: 1,
+						scale: 1,
+					}}
+					transition={{
+						duration: 1.5,
+					}}
+					className="flex flex-row items-center text-gray-300 cursor-pointer "
+				>
+					<div className="tippy-tooltip">
+						<SocialIcon
+							className="text-[#808080ff] transition cursor-pointer hover:text-white "
+							network="email"
+							fgColor="currentColor"
+							bgColor="transparent"
+						/>
+					</div>
+					<p className="hidden text-sm text-gray-400 uppercase md:inline-flex">
+						Get In Touch
+					</p>
+				</motion.div>
+			</Link>
+		</header>
+	);
 }
