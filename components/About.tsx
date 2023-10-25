2dd577c0de6163d3181ea8f7b1a839a825c911a3
Zidane Innis <zidaneinnis@gmail.com>
2023-10-25 01:02:56 -0500

    Adjust about section image size on XL screens

--- components/About.tsx
+++ components/About.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { PageInfo } from "../typings";
-import { urlFor } from "../sanity";
+import React from 'react';
+import { motion } from 'framer-motion';
+import { PageInfo } from '../typings';
+import { urlFor } from '../sanity';
 type Props = { pageInfo: PageInfo };
 
 function About({ pageInfo }: Props) {
-  return (
-    <motion.div className="relative flex flex-col items-center justify-center px-10 mx-auto mb-28 md:text-left md:flex-row max-w-7xl top-6 md:top-0 lg:top-10 ">
-      <h3 className="absolute uppercase top-8  ml-6 tracking-[20px] text-gray-500 text-2xl lg:text-4xl">
-        About
-      </h3>
-      <motion.img
-        initial={{
-          x: -200,
-          opacity: 0,
-        }}
-        transition={{
-          duration: 1.2,
-        }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: true }}
-        src={urlFor(pageInfo?.profilePic).url()}
-        className="flex-shrink-0 object-cover w-56 h-56 rounded-full md:mb-0 md:rounded-3xl md:w-64 md:h-96 xl:w-[500px] xl:h-[600px] mt-20"
-      />
-      <div className="space-y-5 md:px-10 text">
-        <h4 className="text-4xl font-semibold text-center ">
-          Background and Skills
-        </h4>
-        <p className="text-sm lg:text-lg">{pageInfo?.backgroundInformation}</p>
-      </div>
-    </motion.div>
-  );
+	return (
+		<motion.div className="relative flex flex-col items-center justify-center px-10 mx-auto mb-28 md:text-left md:flex-row max-w-7xl top-6 md:top-0 lg:top-10 ">
+			<h3 className="absolute uppercase top-8  ml-6 tracking-[20px] text-gray-500 text-2xl lg:text-4xl">
+				About
+			</h3>
+			<motion.img
+				initial={{
+					x: -200,
+					opacity: 0,
+				}}
+				transition={{
+					duration: 1.2,
+				}}
+				whileInView={{ opacity: 1, x: 0 }}
+				viewport={{ once: true }}
+				src={urlFor(pageInfo?.profilePic).url()}
+				className="flex-shrink-0 object-cover w-56 h-56 rounded-full md:mb-0 md:rounded-3xl md:w-64 md:h-96 xl:w-[400px] xl:h-[500px] mt-20"
+			/>
+			<div className="space-y-5 md:px-10 text">
+				<h4 className="text-4xl font-semibold text-center ">
+					Background and Skills
+				</h4>
+				<p className="text-sm lg:text-lg">{pageInfo?.backgroundInformation}</p>
+			</div>
+		</motion.div>
+	);
 }
 
 export default About;
