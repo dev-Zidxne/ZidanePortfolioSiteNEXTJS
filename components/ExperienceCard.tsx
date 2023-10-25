c612abbf6976939a63ad4b2dab4b960a2b0412db
Zidane Innis <zidaneinnis@gmail.com>
2023-10-25 01:25:41 -0500

    Changed Image component back to regular

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { Experience } from '../typings';
 import { urlFor } from '../sanity';
-import Image from 'next/image';
 
 type Props = {
 	experience: Experience;
@@ -50,10 +49,9 @@ export default function ExperienceCard({ experience }: Props) {
 						? experience.technologies.map(
 								(technology) =>
 									technology?.image && (
-										<Image
+										<img
 											key={technology._id}
 											className="h-10 m-1 rounded-full"
-											alt="technology image"
 											src={urlFor(technology.image).url()}
 										/>
 									)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 }
 export default MyApp;
