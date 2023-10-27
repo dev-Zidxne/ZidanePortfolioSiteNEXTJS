42235baafb3bf9c286ee17c318caf1c0b6df4026
Zidane Innis <zidaneinnis@gmail.com>
2023-10-27 18:25:36 -0500

    fixed horizontal scrolling and about section align

--- components/About.tsx
+++ components/About.tsx
@@ -6,9 +6,9 @@ type Props = { pageInfo: PageInfo };
 
 function About({ pageInfo }: Props) {
 	return (
-		<motion.div className="relative flex flex-col items-center justify-center px-10 mx-auto mb-28 md:text-left md:flex-row max-w-7xl top-6 md:top-0 lg:top-10">
+		<motion.div className="relative flex flex-col items-center justify-center px-10 mx-auto mb-28 md:text-left md:flex-row max-w-7xl top-6 md:top-0 lg:top-10 h-screen">
 			{/* Centered horizontally at the top of the motion.div */}
-			<h3 className="absolute uppercase top-0 left-1/2 transform -translate-x-1/2 tracking-[20px] text-gray-500 text-2xl lg:text-4xl">
+			<h3 className="absolute  uppercase top-6  ml-6 tracking-[20px] text-gray-500 text-2xl lg:top-32 lg:text-4xl sm:top-20 ">
 				About
 			</h3>
 			<motion.img
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,89 +1,89 @@
-import type { GetServerSideProps, GetStaticProps, NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import About from "../components/About";
-import ContactMe from "../components/ContactMe";
-import ExperienceCard from "../components/ExperienceCard";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Projects from "../components/Projects";
-import Skills from "../components/Skills";
-import WorkExperience from "../components/WorkExperience";
-import { urlFor } from "../sanity";
-import styles from "../styles/Home.module.css";
-import { Experience, PageInfo, Project, Skill, Social } from "../typings";
-import { fetchExperience } from "../utils/fetchExperiences";
-import { fetchPageInfo } from "../utils/fetchPageInfo";
-import { fetchProjects } from "../utils/fetchProjects";
-import { fetchSkills } from "../utils/fetchSkills";
-import { fetchSocials } from "../utils/fetchSocials";
+import type { GetServerSideProps, GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import About from '../components/About';
+import ContactMe from '../components/ContactMe';
+import ExperienceCard from '../components/ExperienceCard';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Projects from '../components/Projects';
+import Skills from '../components/Skills';
+import WorkExperience from '../components/WorkExperience';
+import { urlFor } from '../sanity';
+import styles from '../styles/Home.module.css';
+import { Experience, PageInfo, Project, Skill, Social } from '../typings';
+import { fetchExperience } from '../utils/fetchExperiences';
+import { fetchPageInfo } from '../utils/fetchPageInfo';
+import { fetchProjects } from '../utils/fetchProjects';
+import { fetchSkills } from '../utils/fetchSkills';
+import { fetchSocials } from '../utils/fetchSocials';
 
 type Props = {
-  pageInfo: PageInfo;
-  experiences: Experience[];
-  skills: Skill[];
-  projects: Project[];
-  socials: Social[];
+	pageInfo: PageInfo;
+	experiences: Experience[];
+	skills: Skill[];
+	projects: Project[];
+	socials: Social[];
 };
 
 const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
-  return (
-    <div className="bg-[rgb(35,35,35)] text-white h-screen  overflow-y-scroll overscroll-x-scroll   scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
-      <Head>
-        <title>{pageInfo?.name}</title>
-      </Head>
-      <Header socials={socials} />
+	return (
+		<div className="bg-[rgb(35,35,35)] text-white h-screen  overflow-x-hidden   scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 container-snap">
+			<Head>
+				<title>{pageInfo?.name}</title>
+			</Head>
+			<Header socials={socials} />
 
-      <section id="hero" className="snap-start">
-        <Hero pageInfo={pageInfo} />
-      </section>
-      <section id="about" className="snap-center">
-        <About pageInfo={pageInfo} />
-      </section>
-      <section id="experience" className="snap-center">
-        <WorkExperience experiences={experiences} />
-      </section>
-      <section id="skills" className="snap-start">
-        <Skills skills={skills} />
-      </section>
-      <section id="projects" className="snap-start">
-        <Projects projects={projects} />
-      </section>
-      <section id="contact" className="snap-start">
-        <ContactMe pageInfo={pageInfo} />
-      </section>
-      <Link href="#hero">
-        <footer className="sticky w-10 h-10 ml-auto mr-2 cursor-pointer bottom-96">
-          <div className="">
-            <img
-              className="w-8 h-8 rounded-full cursor-pointer filter grayscale hover:grayscale-0 "
-              src={urlFor(pageInfo?.heroImage).url()}
-              alt=""
-            ></img>
-          </div>
-        </footer>
-      </Link>
-    </div>
-  );
+			<section id="hero" className="snap-start">
+				<Hero pageInfo={pageInfo} />
+			</section>
+			<section id="about" className="snap-center">
+				<About pageInfo={pageInfo} />
+			</section>
+			<section id="experience" className="snap-center">
+				<WorkExperience experiences={experiences} />
+			</section>
+			<section id="skills" className="snap-start">
+				<Skills skills={skills} />
+			</section>
+			<section id="projects" className="snap-start">
+				<Projects projects={projects} />
+			</section>
+			<section id="contact" className="snap-start">
+				<ContactMe pageInfo={pageInfo} />
+			</section>
+			<Link href="#hero">
+				<footer className="sticky w-10 h-10 ml-auto mr-2 cursor-pointer bottom-96">
+					<div className="">
+						<img
+							className="w-8 h-8 rounded-full cursor-pointer filter grayscale hover:grayscale-0 "
+							src={urlFor(pageInfo?.heroImage).url()}
+							alt=""
+						></img>
+					</div>
+				</footer>
+			</Link>
+		</div>
+	);
 };
 
 export default Home;
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const pageInfo: PageInfo = await fetchPageInfo();
-  const experiences: Experience[] = await fetchExperience();
-  const skills: Skill[] = await fetchSkills();
-  const projects: Project[] = await fetchProjects();
-  const socials: Social[] = await fetchSocials();
+	const pageInfo: PageInfo = await fetchPageInfo();
+	const experiences: Experience[] = await fetchExperience();
+	const skills: Skill[] = await fetchSkills();
+	const projects: Project[] = await fetchProjects();
+	const socials: Social[] = await fetchSocials();
 
-  return {
-    props: {
-      pageInfo,
-      experiences,
-      skills,
-      projects,
-      socials,
-    },
-  };
+	return {
+		props: {
+			pageInfo,
+			experiences,
+			skills,
+			projects,
+			socials,
+		},
+	};
 };
