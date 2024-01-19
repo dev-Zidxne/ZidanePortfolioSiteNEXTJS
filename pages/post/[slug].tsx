21c56b8b464adc68b8e84f5cfb48d4c2e8c0fc9b
Zidane Innis <zidaneinnis@gmail.com>
2024-01-18 21:53:02 -0500

    Speed improvements

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -25,7 +25,14 @@ const Post = ({ socials, post, pageInfo }: Props) => {
 	const { slug } = router.query;
 	const baseUrl = 'https://devzidane.vercel.app';
 	const fullUrl = slug ? `${baseUrl}/post/${slug}` : baseUrl;
-
+	if (!post || !post.title) {
+		return (
+			<>
+				{/* Add any necessary head data here */}
+				<p>Loading post data or post not found...</p>
+			</>
+		);
+	}
 	const {
 		title = 'Missing title',
 		name = 'Missing name',
@@ -55,14 +62,6 @@ const Post = ({ socials, post, pageInfo }: Props) => {
 			)}
 		</Head>
 	);
-	if (!post) {
-		return (
-			<>
-				{pageHeadData}
-				<p>Loading post data or post not found...</p>
-			</>
-		);
-	}
 
 	return (
 		<div className="bg-[rgb(35,35,35)] flex-grow text-white h-screen overflow-x-hidden    scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80  flex flex-col min-h-screen ">
